fc764890a121a68208011543bed616f7b21eb2a9
Alexandr Gindyuk <alexandr.gindyuk@gmail.com>
2021-08-02 11:04:52 +0300

    DRAFT-000:EDIT: Bugfix for carousel

--- src/js/widgets/global/Swiper.js
+++ src/js/widgets/global/Swiper.js
@@ -4,22 +4,23 @@ import Swiper from 'swiper/swiper-bundle';
 var swiperSliders = document.querySelectorAll('.swiper-container');
 
 // Default options for swiper
-var swiperDefaultOptions = {
-    navigation: {
-        disabledClass: 'm-disabled',
-        nextEl: '.button-arrow.m-right',
-        prevEl: '.button-arrow.m-left'
-    },
-    slidesPerView: 1,
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 40
-        }
-      }
-}
+var swiperDefaultOptions = {};
 
 swiperSliders.forEach(el => {
-    var options = el.dataset.swiperOptions ? Object.assign(swiperDefaultOptions, JSON.parse(el.dataset.swiperOptions)) : swiperDefaultOptions
+    swiperDefaultOptions = {};
+    // Combine Default options for swiper with selector that can be outside the swiper-container (for example Arrows)
+    Object.assign(swiperDefaultOptions, {
+        navigation: {
+            disabledClass: 'm-disabled',
+            nextEl: el.closest('.slot-swiper').querySelector('.button-arrow.m-right'),
+            prevEl: el.closest('.slot-swiper').querySelector('.button-arrow.m-left')
+        },
+        scrollbar: {
+            el: el.closest('.slot-swiper').querySelector('.swiper-scrollbar')
+        },
+    });
+
+    var options = el.dataset.swiperOptions ? Object.assign(swiperDefaultOptions, JSON.parse(el.dataset.swiperOptions)) : swiperDefaultOptions;
+    console.log(options);
     new Swiper(el, options);
 })
